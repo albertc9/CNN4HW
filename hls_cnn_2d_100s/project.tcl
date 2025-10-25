dbc245a5d7acaa4ffb0d29ff67d2c629e2b49c6b
albert_fedora <albert.chg9@gmail.com>
2025-10-24 17:35:53 -0700

    ready to generate prj

---
+++ hls_cnn_2d_100s/project.tcl
@@ -0,0 +1,12 @@
+variable project_name
+set project_name "hls_cnn_2d_100s"
+variable backend
+set backend "vivado"
+variable part
+set part "xcku5p-ffvb676-2-e"
+variable clock_period
+set clock_period 5
+variable clock_uncertainty
+set clock_uncertainty 12.5%
+variable version
+set version "1.0.0"
